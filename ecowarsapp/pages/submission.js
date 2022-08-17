fced75cfe7cac0b7ec53807d14687d2c2fd9c811
Adam P <adam.phasey@hotmail.com>
2022-08-17 12:02:09 +0100

    Added Nextjs and file structures

---
+++ ecowarsapp/pages/submission.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Submission() {
+  return (
+    <h1>
+        Submission Page
+    </h1>
+  )
+}
